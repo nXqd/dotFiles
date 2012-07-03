e8c959daf6ddf767a44de5014efb1458093c409b
nXqd <nxqd.inbox@gmail.com>
2012-07-03 09:02:02 +0200

    merge from master

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,49 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-git pull
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" --exclude ".gitsubmodules" --exclude ".gitignore" -av . ~
+#!/bin/zsh
+# newbootstrap.sh
+# author: nXqd
+# vim: set ts=2:sw=2
+#############################
+
+walk() {
+  cmd=$1
+
+  dotFiles=(`ls -da .* | grep -Pxv '\.git|\.+'`)
+  curDir=$(pwd)
+  homeDir=(`cd ~;pwd`)
+
+  for i in ${dotFiles[*]}; do
+    target=$curDir"/"$i
+    linkName=$homeDir"/"$i
+
+    $cmd
+  done
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-  doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-    doIt
-	fi
+
+install() {
+  if [ -L $linkName ]; then
+    echo "Remove file or symlink: "$linkName
+    rm -rf $linkName
+  fi
+  ln -s $target $linkName
+}
+
+uninstall() {
+  if [ -L $linkName ]; then
+    echo "Remove file or symlink: "$linkName
+    rm -rf $linkName
+  fi
+}
+
+cmd="$1"
+if [ -z "$cmd" ]; then
+    echo "Usage: go [command]"
+    exit 1
 fi
-unset doIt
-zsh ~/.zshrc
+
+case "$cmd" in
+    install)        walk install;;
+    uninstall)      walk uninstall;;
+    *) echo "Unknown command $cmd"
+       exit 1
+       ;;
+esac
